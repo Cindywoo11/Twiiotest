e64ddfc7d71632cdbdf491bfa02edc6f56f3fe0f
kylalimza <82747290+kylalimza@users.noreply.github.com>
2021-07-04 16:57:52 +0800

    i changed your body message

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ auth_token = os.environ['auth_token']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='THIS is MY notification from GitHub via Twilio-Please check changes',
+                              body='Amended something on cindywoo11/twiliotest repository',
                               from_='whatsapp:+14155238886',
                               to='whatsapp:'+os.environ['to_whatsapp_no']
                           )
